b5945e9507e9f7746e799e9bb62bedffcc14fb94
Febrian Dwi Kimhan <febrian.dwi11@ui.ac.id>
2022-09-14 20:59:49 +0700

    Commit tugas 2 pbp

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_katalog,
+        'nama': 'Febrian Dwi Kimhan',
+        'npm': '2106706691'
+    }
+    return render(request, "katalog.html", context)
